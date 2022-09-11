cc474f416265a1038594bda43370b25cc453268e
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2022-09-11 11:36:36 +0300

    add task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,11 @@
-"use strict";
-
 const totalCategories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${totalCategories.length}`);
 
 const categoriesArray = [...totalCategories]
 .map(
-    category => `Category: ${category.children[0].textContent}
+    category => `Category: ${category.firstElementChild.textContent}
 Elements: ${category.children[1].children.length}`
 )
 .join("\n");
 console.log(categoriesArray);
+
